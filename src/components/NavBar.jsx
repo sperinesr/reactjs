3871176db4e6b18ea2afede975117617b982fc71
Sebastian Perines <sebastianperinesr@gmail.com>
2023-08-30 21:31:37 -0400

    Correccion en redirecciones

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -5,18 +5,19 @@ import { NavDropdown } from 'react-bootstrap';
 
 import { CartWidget } from "./CartWidget";
 
+import { Link } from 'react-router-dom';
+
 export const NavBar = ({ categories }) => (
     <header>
         <Navbar bg="dark" data-bs-theme="dark">
             <Container>
                 <Navbar.Brand href="/">TCGstore</Navbar.Brand>
                 <Nav className="me-auto">
-                    <Nav.Link href="/">Inicio</Nav.Link>
                     <NavDropdown title="Categorias" id="basic-nav-dropdown">
-                        <NavDropdown.Item href="/category/men's clothing">men's clothing</NavDropdown.Item>
-                        <NavDropdown.Item href="/category/jewelery">jewelery</NavDropdown.Item>
-                        <NavDropdown.Item href="/category/electronics">electronics</NavDropdown.Item>
-                        <NavDropdown.Item href="/category/women's clothing">women's clothing</NavDropdown.Item>
+                        <NavDropdown.Item as={Link} to="/category/men's clothing">men's clothing</NavDropdown.Item>
+                        <NavDropdown.Item as={Link} to="/category/jewelery">jewelery</NavDropdown.Item>
+                        <NavDropdown.Item as={Link} to="/category/electronics">electronics</NavDropdown.Item>
+                        <NavDropdown.Item as={Link} to="/category/women's clothing">women's clothing</NavDropdown.Item>
                     </NavDropdown>
                     <Nav.Link href="#nosotros">Nosotros</Nav.Link>
                 </Nav>
